060d927fe5db8ede4041dbc2a6e59c6421a4ee0d
Leyla Berhuş <leylaberhus@gmail.com>
2026-05-19 02:09:06 +0300

    Veritabanı seed datası eklendi ve resimler eşitlendi

--- Data/UygulamaDbContext.cs
+++ Data/UygulamaDbContext.cs
@@ -13,9 +13,29 @@ namespace EmekSepetiWeb.Data
         public DbSet<SepetElemani> SepetElemanlari { get; set; }
 
         public DbSet<Siparis> Siparisler { get; set; }
-        
+
 
 
         public DbSet<Kategori> Kategoriler { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Kategori>().HasData(
+                new Kategori { Id = 1, Ad = "Ev Yemekleri & Gıda" },
+                new Kategori { Id = 2, Ad = "Takı & Aksesuar" },
+                new Kategori { Id = 3, Ad = "Ahşap & Oyuncak" }
+            );
+
+            modelBuilder.Entity<Urun>().HasData(
+                // Hepsine örnek de olsa bir Aciklama değeri ekledik:
+                new Urun { Id = 1, Ad = "Kayseri Usulü Mantı", Aciklama = "Ev yapımı nefis Kayseri mantısı", Fiyat = 350.00m, ResimUrl = "manti.jpg", KategoriId = 1, OlusturmaTarihi = new DateTime(2026, 05, 17) },
+                new Urun { Id = 2, Ad = "Doğal Taşlı Kolye", Aciklama = "Özel tasarım doğal taşlı şık kolye", Fiyat = 190.00m, ResimUrl = "kolye.jpg", KategoriId = 2, OlusturmaTarihi = new DateTime(2026, 05, 17) },
+                new Urun { Id = 3, Ad = "Ahşap Oyuncak Tren", Aciklama = "Çocuklar için sağlıklı ahşap oyuncak tren", Fiyat = 280.00m, ResimUrl = "tren.jpg", KategoriId = 3, OlusturmaTarihi = new DateTime(2026, 05, 17) },
+                new Urun { Id = 8, Ad = "El Örgüsü Atkı", Aciklama = "Gri ve günlük kullanıma uygun el örgüsü atkı", Fiyat = 149.99m, ResimUrl = "el örgüsü atkı.png", KategoriId = 2, OlusturmaTarihi = new DateTime(2026, 05, 17) },
+                new Urun { Id = 9, Ad = "Tahta Pinokyo", Aciklama = "Ürün tahtadan yapılmıştır el emeğidir", Fiyat = 500.00m, ResimUrl = "tahta pinokyo.png", KategoriId = 3, OlusturmaTarihi = new DateTime(2026, 05, 17) }
+            );
+        }
     }
 }
---
+++ Migrations/20260518224025_SeedVerileriEklendi.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmekSepetiWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVerileriEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Kategoriler",
+                columns: new[] { "Id", "Ad" },
+                values: new object[,]
+                {
+                    { 1, "Ev Yemekleri & Gıda" },
+                    { 2, "Takı & Aksesuar" },
+                    { 3, "Ahşap & Oyuncak" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Urunler",
+                columns: new[] { "Id", "Aciklama", "Ad", "Fiyat", "KategoriId", "OlusturmaTarihi", "ResimUrl", "UygulamaKullanicisiId" },
+                values: new object[,]
+                {
+                    { 1, "Ev yapımı nefis Kayseri mantısı", "Kayseri Usulü Mantı", 350.00m, 1, new DateTime(2026, 5, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "manti.jpg", null },
+                    { 2, "Özel tasarım doğal taşlı şık kolye", "Doğal Taşlı Kolye", 190.00m, 2, new DateTime(2026, 5, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "kolye.jpg", null },
+                    { 3, "Çocuklar için sağlıklı ahşap oyuncak tren", "Ahşap Oyuncak Tren", 280.00m, 3, new DateTime(2026, 5, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "tren.jpg", null },
+                    { 8, "Gri ve günlük kullanıma uygun el örgüsü atkı", "El Örgüsü Atkı", 149.99m, 2, new DateTime(2026, 5, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "el örgüsü atkı.png", null },
+                    { 9, "Ürün tahtadan yapılmıştır el emeğidir", "Tahta Pinokyo", 500.00m, 3, new DateTime(2026, 5, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "tahta pinokyo.png", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Urunler",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Urunler",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Urunler",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Urunler",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Urunler",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Kategoriler",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Kategoriler",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Kategoriler",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
